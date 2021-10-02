f35b865c45e9b252a399678ce31c826de90830b6
gougou11 <cinherit@sina.com>
2021-10-02 11:15:58 +0800

    add some files

---
+++ Cpptime.cpp
@@ -0,0 +1,15 @@
+#include "Cpptime.h"
+#include <iostream>
+using namespace std;
+int main() {
+    Time start;
+    for (int i = 1; i < 3000; i++) {
+        cout << i << ' ';
+        if (i % 10 == 0) cout << endl;
+    }
+    Time end;
+    cout << endl;
+    cout << "start = " << start.ascii();
+    cout << "end = " << end.ascii();
+    cout << "delta = " << end.delta(&start);
+}
