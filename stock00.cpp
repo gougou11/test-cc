c5195a6ceffc8d98ec6284cc480f7dbc13533d57
gougou11 <cinherit@sina.com>
2021-10-27 23:23:55 +0800

    add some files

---
+++ stock00.cpp
@@ -0,0 +1,19 @@
+// version 00
+#include <iostream>
+#include "stock00.h"
+
+void Stock::acquire(const std::string & co, long n, double pr)
+{
+    company = co;
+    if (n < 0)
+    {
+        std::cout << "Number of shares can't be negative; "
+                  << company << " shares set to 0.\n";
+        shares = 0;
+    } else 
+        shares = n;
+    
+    share_val = pr;
+    set_tot();
+}
+
